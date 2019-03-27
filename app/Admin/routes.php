052fa28560b58ece20bab0c746284e76c94acd5a
Luke <tset@email.com>
2019-03-27 18:00:30 +0800

    update admin/about page

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -30,4 +30,6 @@ Route::group([
     $router->get('coupon_codes/{id}/edit', 'CouponCodesController@edit');
     $router->put('coupon_codes/{id}', 'CouponCodesController@update');
     $router->delete('coupon_codes/{id}', 'CouponCodesController@destroy');
+
+    $router->get('about', 'AboutController@index');
 });
